1f23bad7a8f4e01293fa411c5f1a003e1ffa98b3
Matthew Bashton <matthew.bashton@newcastle.ac.uk>
2017-06-12 14:42:01 +0100

    Added sorted output back in

--- grolar.R
+++ grolar.R
@@ -150,7 +150,7 @@ GetFusionz_and_namez <- function(sample, suffix) {
   # Sort
   cat("Sorting by number of events\n")
   idx <- order(JSON_level1$splitcount, JSON_level1$paircount, decreasing = TRUE)
-  output[idx,]
+  output <- output[idx,]
 
   # Write out output
   cat(paste0("Writing out table: ", sample, "_fusions_filt_sorted.txt", "\n"))
