532147ba88e45a3cf58437bd36e5cf529324d2ff
Matthew Bashton <matthew.bashton@newcastle.ac.uk>
2017-07-24 16:31:02 +0100

    Dynamically sets fragment length now for each sample

--- Make_job_list_pizzly.sh
+++ Make_job_list_pizzly.sh
@@ -6,6 +6,8 @@
 
 # This run list is then supplied to GNU parallel --jobs
 
+# Needs Pizzly script get_fragment_length.py                                                                                                                                                                                                                                            
+
 hostname
 date
 
@@ -18,24 +20,26 @@ OUTPUT=$2
 
 # Set counter
 COUNT=1
-END=$(wc -l $INPUT_LIST | awk '{print $1}' | grep -oP '^\S+')
+END=$(wc -l $INPUT_LIST | awk '{print $1}')
 
 echo " "
 echo " * Input file is: $INPUT_LIST"
 echo " * Number of runs: $END"
-echo " * Output job list for GNU parallel saved to: $OUTPUT"
+echo " * Output for GNU parallel saved to: $OUTPUT"
 echo " "
 
 # Main bit of command-line for job
-CMD="pizzly -k 31 -a 2 --insert-size 400 --fasta Homo_sapiens.GRCh38.cdna.all.fa --gtf Homo_sapiens.GRCh38.89.gtf --output"
+CMD="pizzly -k 31 -a 2 --insert-size"
 
 # Main Loop
 [ -e $OUTPUT ] && rm $OUTPUT
 while [ $COUNT -le $END ];
 do
-    LINE=$(awk "NR==$COUNT" $INPUT_LIST)
+    LINE=$(awk "NR==$COUNT" $INPUT_LIST | grep -oP '^\S+')
+    # Get insert size from python script
+    SIZE=$(python /usr/local/bin/get_fragment_length.py ${LINE}/abundance.h5)
     # Make file list
-    echo "Working on $COUNT of $END Sample ID: $LINE"
-    echo "$CMD ${LINE}_fusion_GRCh38 ${LINE}/fusion.txt" >> $OUTPUT
+    echo "Working on $COUNT of $END Sample ID: $LINE, insert size is: $SIZE"
+    echo "$CMD $SIZE --fasta Homo_sapiens.GRCh38.cdna.all.fa --gtf Homo_sapiens.GRCh38.89.gtf --output ${LINE}_fusion_GRCh38 ${LINE}/fusion.txt" >> $OUTPUT
     ((COUNT++))
 done
